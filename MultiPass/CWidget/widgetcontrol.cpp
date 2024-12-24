18e8b4ede431a7010486f57e8a82dabcc1821f81
feed999 <feed228channel@gmail.com>
2024-12-24 13:43:47 +0300

    update

--- MultiPass/CWidget/widgetcontrol.cpp
+++ MultiPass/CWidget/widgetcontrol.cpp
@@ -1,30 +1,36 @@
 #include "widgetcontrol.h"
-
+#include "../tools/settings.h"
 // WidgetControl::WidgetControl() {}
 
-WidgetControl::WidgetControl(QWidget *parent) : QDialog(parent) {
-    // Устанавливаем заголовок окна
-    setWindowTitle("Введите значение");
 
-    // Создаем и настраиваем виджеты
+WidgetControl::WidgetControl(QWidget *parent) : QDialog(parent) {
+    setWindowTitle("Введите значение");
+    setStyleSheet(
+        "QDialog { background-color: rgb(57, 62, 70); }" // Фон диалога
+        "QLabel { color: #FFFFFF; font-size: 16px; font-weight: bold; }" // Стиль текста
+        "QLineEdit { background-color: #23272A; color: #FFFFFF; font-size: 14px; padding: 5px; border: 2px solid #7289DA; border-radius: 5px; }" // Поле ввода
+        "QPushButton { background-color: #7289DA; color: #FFFFFF; font-size: 14px; font-weight: bold; border: none; padding: 10px; border-radius: 5px; }"
+        "QPushButton:hover { background-color: #5B6EAE; }" // Эффект при наведении
+        "QPushButton:pressed { background-color: #4E5E96; }" // Эффект при нажатии
+        );
+// parent->setStyleSheet(QString::fromUtf8(INTERFACE::CSS_BACKGROUND_COLOR));
     QVBoxLayout *layout = new QVBoxLayout(this);
+
     QLabel *label = new QLabel("Введите текст:", this);
     QLineEdit *inputField = new QLineEdit(this);
     QPushButton *okButton = new QPushButton("Ок", this);
     QPushButton *cancelButton = new QPushButton("Отмена", this);
 
-    // Добавляем виджеты в компоновку
     layout->addWidget(label);
     layout->addWidget(inputField);
     layout->addWidget(okButton);
     layout->addWidget(cancelButton);
 
-    // Соединяем сигналы и слоты
     connect(okButton, &QPushButton::clicked, this, &QDialog::accept);
     connect(cancelButton, &QPushButton::clicked, this, &QDialog::reject);
 
-    // Устанавливаем модальность диалога
     setModal(true);
+
 }
 
 QString WidgetControl::getInput()
